5cb18b9ced80c06a0fa52c7b04ccfd2a22938d65
omar <omarahmed2003eg@gmail.com>
2024-05-18 04:26:14 +0300

    added instructor Login

---
+++ middleware/instructorAuth.js
@@ -0,0 +1,22 @@
+const jwt = require("jsonwebtoken");
+const connection = require("../utils/db");
+const instructorAuth = async (req, res, next) => {
+  const { authorization } = req.headers;
+  if (!authorization) {
+    return res.status(401).json({ error: "Authorization token required" });
+  }
+  const token = authorization.split(" ")[1];
+  try {
+    const { id } = jwt.verify(token, process.env.SECRET);
+    const query = `select * from instructor where id = ?`;
+    const [user] = await connection.promise().query(query, [id]);
+    if (user.length === 0) {
+      throw new Error("Request is not authorized");
+    }
+    req.user = user[0].id;
+    next();
+  } catch (error) {
+    res.status(401).json({ error: error.message });
+  }
+};
+module.exports = instructorAuth;
